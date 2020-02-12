de43737166c4684346d71dd76d699eeb95938ed7
devin-singh <59748819+devin-singh@users.noreply.github.com>
2020-02-12 14:23:19 -0700

    Revert "Started Model Controller"

--- TicTacToeGroup/TicTacToeGroup/Controllers/Model Controllers/TicTacToeController.swift
+++ TicTacToeGroup/TicTacToeGroup/Controllers/Model Controllers/TicTacToeController.swift
@@ -7,24 +7,3 @@
 //
 
 import Foundation
-
-class TicTacToeController {
-    
-    // MARK: - Properties
-    
-    static let shared = TicTacToeController()
-    
-    // TODO: - Change Int to custom Symbol type
-    
-    var ticTacToeBoard = [[Int]](repeating: [Int](repeating: 0, count: 3), count: 3)
-    
-    // MARK: - Property Maniputation Functions
-    
-    func resetBoard() {
-        for (r, array) in ticTacToeBoard.enumerated() {
-            for (c, _) in array.enumerated() {
-                ticTacToeBoard[r][c] = 0
-            }
-        }
-    }
-}
