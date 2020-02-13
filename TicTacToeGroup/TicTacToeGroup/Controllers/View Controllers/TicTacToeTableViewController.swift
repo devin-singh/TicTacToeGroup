8a3e8d770e3052e9d995ebe99c12da1e5f4243e4
Devin Singh <notpublic@gmail.com>
2020-02-12 20:08:48 -0700

    Added basic functionality

--- TicTacToeGroup/TicTacToeGroup/Controllers/View Controllers/TicTacToeTableViewController.swift
+++ TicTacToeGroup/TicTacToeGroup/Controllers/View Controllers/TicTacToeTableViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class TicTacToeTableViewController: UITableViewController {
     
-    //MARK - Properties
-    let buttonBoard = [[UIButton]](repeating: [UIButton](repeating: UIButton(), count: 3), count: 3)
+    // MARK: - Properties
     
-    //MARK - OUTLETS
+    var buttonBoard = [[UIButton]](repeating: [UIButton](repeating: UIButton(), count: 3), count: 3)
+    
+    // MARK: - Outlets
     
     @IBOutlet weak var gameStateLabel: UILabel!
     @IBOutlet weak var gridButton00: UIButton!
@@ -33,33 +34,71 @@ class TicTacToeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initButtonBoard()
     }
-    //MARK - Private Functions
-    private func initButtonBoard() {
-        
-    }
-    //MARK - ACTIONS
+    //MARK: - Private functions
     
+    private func initButtonBoard() {
+        self.buttonBoard = [[gridButton00, gridButton01, gridButton02],
+                            [gridButton10, gridButton11, gridButton12],
+                            [gridButton20, gridButton21, gridButton22]]
+    }
+    
+    private func updateView(row: Int, column: Int) {
+        TicTacToeController.shared.takeTurn(row: row, column: column) { (gamestate) in
+            DispatchQueue.main.async {
+                switch gamestate {
+                case .turnTaken:
+                    self.buttonBoard[row][column].setImage(UIImage(named: Symbol.getImageName(symbol: TicTacToeController.shared.turn)), for: .normal)
+                case .tie:
+                    self.buttonBoard[row][column].setImage(UIImage(named: Symbol.getImageName(symbol: TicTacToeController.shared.turn)), for: .normal)
+                    self.gameStateLabel.text = "Tie Game!"
+                    TicTacToeController.shared.turn = -1
+                case .winner:
+                    self.buttonBoard[row][column].setImage(UIImage(named: Symbol.getImageName(symbol: TicTacToeController.shared.turn)), for: .normal)
+                    let symbol = TicTacToeController.shared.turn
+                    self.gameStateLabel.text = "\(Symbol.getWinner(symbol: symbol))"
+                    TicTacToeController.shared.turn = -1
+                case .error:
+                    self.gameStateLabel.text = "Error with Model Controller Logic"
+                    TicTacToeController.shared.turn = -1
+                }
+            }
+            
+        }
+    }
+    
+    //MARK: - Actions
     
     @IBAction func gridButton00Tapped(_ sender: Any) {
+        updateView(row: 0, column: 0)
     }
     @IBAction func gridButton01Tapped(_ sender: Any) {
+        updateView(row: 0, column: 1)
     }
     @IBAction func gridButton02Tapped(_ sender: Any) {
+        updateView(row: 0, column: 2)
     }
     @IBAction func gridButton10Tapped(_ sender: Any) {
+        updateView(row: 1, column: 0)
     }
     @IBAction func gridButton11Tapped(_ sender: Any) {
+        updateView(row: 1, column: 1)
     }
     @IBAction func gridButton12Tapped(_ sender: Any) {
+        updateView(row: 1, column: 2)
     }
     @IBAction func gridButton20Tapped(_ sender: Any) {
+        updateView(row: 2, column: 0)
     }
     @IBAction func gridButton21Tapped(_ sender: Any) {
+        updateView(row: 2, column: 1)
     }
     @IBAction func gridButton22Tapped(_ sender: Any) {
+        updateView(row: 2, column: 2)
     }
     @IBAction func menuButtonTapped(_ sender: Any) {
+        
     }
     @IBAction func newGameButtonTapped(_ sender: Any) {
     }
